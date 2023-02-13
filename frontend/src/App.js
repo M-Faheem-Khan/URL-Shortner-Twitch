ad85f1067cce35e4ae01e24e1b5222317561471e
M-Faheem-Khan <faheem5948@gmail.com>
2023-02-12 18:45:27 -0700

    fix input field width
    Signed-off-by: M-Faheem-Khan <faheem5948@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ const App = () => {
   return (
     <div className="grid h-screen place-items-center">
       <section className="bg-white dark:bg-gray-900">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 w-96">
           <div className="mx-auto max-w-screen-md sm:text-center">
             <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">Shorten URL</h2>
             <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Links live for 24 hours.</p>
